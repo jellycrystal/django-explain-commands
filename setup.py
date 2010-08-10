87041307327be9a77f9a7c74e388c2bccca89fb3
Yury Yurevich <yyurevich@jellycrystal.com>
2010-08-10 20:55:01 +0700

    Initial import.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='django-explain-commands',
+    version='0.1',
+    description='Django app for explaining where commands are coming from.',
+    long_description=open('README.rst').read(),
+    author='Yury Yurevich',
+    author_email='yyurevich@jellycrystal.com',
+    url='https://github.com/j2a/django-explain-command',
+    license='BSD',
+    include_package_data=True,
+    zip_safe=True,
+    # Get more strings from http://www.python.org/pypi?:action=list_classifiers
+    classifiers=[
+            'Development Status :: Beta',
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
+
