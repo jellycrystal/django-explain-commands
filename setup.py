ec474dc5e61a9152b6af08547ab6db0334fc0f4e
Yury Yurevich <yyurevich@jellycrystal.com>
2010-08-11 22:56:31 +0700

    Make setup.py fully working. Both with sdist and install.

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-explain-commands',
-    version='0.1',
+    version='0.1.1',
     description='Django app for explaining where commands are coming from.',
     long_description=open('README.md').read(),
     author='Yury Yurevich',
     author_email='yyurevich@jellycrystal.com',
     url='https://github.com/jellycrystal/django-explain-commands',
     license='BSD',
-    include_package_data=True,
+    packages=find_packages(),
+    include_package_data=False,
     zip_safe=True,
     # Get more strings from http://www.python.org/pypi?:action=list_classifiers
     classifiers=[
