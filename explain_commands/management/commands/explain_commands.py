f0b53f33a4cd06d9906376dd11090cf7c9b7d3a6
Yury Yurevich <yyurevich@jellycrystal.com>
2010-08-13 16:18:20 +0000

    Show short help for each command.

--- explain_commands/management/commands/explain_commands.py
+++ explain_commands/management/commands/explain_commands.py
@@ -1,4 +1,4 @@
-from django.core.management import base, get_commands, color
+from django.core.management import base, get_commands, load_command_class, color
 from django.utils import termcolors
 
 def color_style():
@@ -6,6 +6,7 @@ def color_style():
     style.PROVIDER = termcolors.make_style(fg='green', opts=('bold',))
     style.COMMAND = termcolors.make_style(opts=('bold',))
     style.UNKNOWN = termcolors.make_style(fg='red')
+    style.HELP = termcolors.make_style()
     return style
 
 def group_commands():
@@ -13,13 +14,16 @@ def group_commands():
     for command, provider in get_commands().items():
         if isinstance(provider, basestring):
             key = provider
+            cls = load_command_class(provider, command)
         elif isinstance(provider, base.BaseCommand):
+            cls = provider
             mod = provider.__module__
             idx = mod.find('management') - 1
             key = mod[:idx]
         else:
             key = '?unknown'
-        grouped[key] = sorted(grouped.get(key, []) + [command])
+        value = (command, getattr(cls, 'help', ''))
+        grouped[key] = sorted(grouped.get(key, []) + [value])
     return grouped
 
 
@@ -31,8 +35,15 @@ def print_commands(grouped):
         else:
             pstyle = style.PROVIDER
         print pstyle(provider) + " =>"
-        for command in grouped[provider]:
-            print "\t" + style.COMMAND(command)
+        for command, helpstr in grouped[provider]:
+            if len(helpstr) > 50:
+                helpstr = helpstr[:47] + '...'
+            if helpstr:
+                helpstr = ' -- ' + helpstr
+
+            print "\t" + \
+                style.COMMAND(command) + \
+                style.HELP(helpstr)
 
 def explain_commands():
     print_commands(group_commands())
